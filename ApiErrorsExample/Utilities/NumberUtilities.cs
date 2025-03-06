a9790b0c2c08f17b04568ff9ab2047527fa1b70a
Jarosław Mielewski <39311561+szarlih@users.noreply.github.com>
2025-03-06 12:32:30 +0100

    Add files via upload

--- ApiErrorsExample/Utilities/NumberUtilities.cs
+++ ApiErrorsExample/Utilities/NumberUtilities.cs
@@ -1,1 +1,10 @@
-
+﻿namespace ApiErrorsExample.Utilities
+{
+    public static class NumberUtilities
+    {
+        public static bool IsInRange(this int number, int lowerBound, int upperBound)
+        {
+            return number >= lowerBound && number <= upperBound;
+        }
+    }
+}
