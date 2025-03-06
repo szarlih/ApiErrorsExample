39626da210889eccf7cc044d86316e75993d0c16
Jarosław Mielewski <39311561+szarlih@users.noreply.github.com>
2025-03-06 12:28:49 +0100

    Add files via upload

--- ApiErrorsExample/Program.cs
+++ ApiErrorsExample/Program.cs
@@ -1,1 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI; // Add this using directive
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
